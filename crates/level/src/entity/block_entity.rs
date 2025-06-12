fb3886ce75a09f5ef0222cfd81f13b799a06721d
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2025-06-12 17:47:24 -0600

    Dexrn you forgor

--- crates/level/src/entity/block_entity.rs
+++ crates/level/src/entity/block_entity.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
-use quartz_nbt::NbtCompound;
 use lodestone_common::types::hashmap_ext::Value;
+use quartz_nbt::NbtCompound;
+use std::collections::HashMap;
 
 pub struct BlockEntity {
     pub id: u16,
@@ -8,7 +8,7 @@ pub struct BlockEntity {
     pub y: i32,
     pub z: i32,
     pub components: NbtCompound, // see line 13's comment
-    pub keep_packed: bool, // apparently whether the block entity should NOT be placed
+    pub keep_packed: bool,       // apparently whether the block entity should NOT be placed
 
-    pub data: HashMap<String, Value> // TODO: should we store raw nbt in here or actual usable values
+    pub data: HashMap<String, Value>, // TODO: should we store raw nbt in here or actual usable values
 }
--- crates/level/src/lib.rs
+++ crates/level/src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(int_roundings)]
 
 pub mod block;
-pub mod level;
 pub mod entity;
+pub mod level;
