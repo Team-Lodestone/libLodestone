d6427c211fb725752d80a21d4742aafd16e556df
Dexrn ZacAttack <me@dexrn.me>
2025-10-16 20:13:35 -0700

    fix(common): add ifdef for demangling, remove useless source file

--- projects/Common/src/String/OperatorStringBuilder.cpp
+++ projects/Common/src/String/OperatorStringBuilder.cpp
@@ -4,12 +4,16 @@
 #include "String/OperatorStringBuilder.h"
 
 constexpr const char *OperatorStringBuilder::demangle(const char *name) {
+#if defined(__GNUC__) || defined(__clang__)
     int err = 0;
     char* demangled = abi::__cxa_demangle(name, nullptr, nullptr, &err);
 
     const char *r = (!err) ? demangled : name;
     std::free(demangled);
     return r;
+#else
+    return name;
+#endif
 }
 
 OperatorStringBuilder::OperatorStringBuilder(const std::type_info &type) : mType(type) {
--- projects/Level/src/Types/Colors.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by DexrnZacAttack on 10/16/25 using zPc-i2.
-//
-#include "Types/Color.h"
