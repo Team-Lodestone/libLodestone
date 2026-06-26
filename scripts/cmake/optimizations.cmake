e25494d3b46acf252479b16a439b571c1fca01e1
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2026-06-25 22:56:08 -0600

    chore(emscripten): link statically for web builds

--- scripts/cmake/optimizations.cmake
+++ scripts/cmake/optimizations.cmake
@@ -1,8 +1,15 @@
 option(ENABLE_OPTIMIZATIONS "Enable compiler optimizations" ON)
 if (ENABLE_OPTIMIZATIONS STREQUAL "ON")
     message(NOTICE "Compiler optimizations enabled")
-    add_compile_options(
-            "$<$<COMPILE_LANGUAGE:CXX>:-O3;-ffast-math;-march=native;-ftree-vectorize>"
-    )
+
+    if (CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
+        add_compile_options(
+                "$<$<COMPILE_LANGUAGE:CXX>:-O3;-ffast-math;-ftree-vectorize>"
+        )
+    else()
+        add_compile_options(
+                "$<$<COMPILE_LANGUAGE:CXX>:-O3;-ffast-math;-march=native;-ftree-vectorize>"
+        )
+    endif()
 endif ()
 unset(ENABLE_OPTIMIZATIONS CACHE)
